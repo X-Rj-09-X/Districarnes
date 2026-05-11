42c1716085ee322082a8c67841372ab108c6dfcc
X-Rj-09-X <rikardojfp@hotmail.com>
2026-05-11 03:27:03 -0500

    importar loguin a main

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -3,11 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package principal;
+import vista.Login;
 
 /**
  *
  * @author Ricardo J
  */
 public class Main {
+    public static void main(String[] args) {
+
+        new Login();
+    }
+    
     
 }
